bd61a55445b5d2d2f809b77785f0a0c44c34bb5a
Eduard <eduardgusak552@gmail.com>
2024-12-26 15:25:06 +0200

    L4VA-19: added auth by x-api-key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
@@ -7,10 +7,17 @@ import { AppService } from './app.service';
 import { VaultModule } from './modules/vault/vault.module';
 
 import { databaseConfig } from './config/database.config';
+import { AuthMiddleware } from './middlewares/auth.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(databaseConfig), VaultModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .forRoutes('*');
+  }
+}
