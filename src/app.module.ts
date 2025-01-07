3d1afe5273461d7831afa3ca52ff5a9e2fd4beb0
YVeselovskyi <19dynamokyiv94@gmail.com>
2025-01-07 11:26:35 +0200

    CHANGED: src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,10 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { VaultModule } from './modules/vault/vault.module';
-
-import { databaseConfig } from './config/database.config';
-import { AuthMiddleware } from './middlewares/auth.middleware';
-
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), VaultModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/modules/asset/asset.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-
-@ApiTags('Asset')
-@Controller('asset')
-export class AssetController {
-
-}
