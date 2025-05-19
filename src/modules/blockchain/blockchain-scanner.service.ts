f130e28eb700702cf202831ef11f80b8fd60cf92
Anton Kotyk <kotik.anton.md@gmail.com>
2025-05-19 13:02:01 +0300

    Run CI a

--- src/modules/blockchain/blockchain-scanner.service.ts
+++ src/modules/blockchain/blockchain-scanner.service.ts
@@ -17,7 +17,6 @@ const DEFAULT_MAX_DELAY_MS = 30000; // 30 seconds
 export class BlockchainScannerService {
   private readonly logger = new Logger(BlockchainScannerService.name);
   private readonly scannerUrl: string;
-
   private readonly scannerKey: string;
 
   constructor(private readonly configService: ConfigService) {
