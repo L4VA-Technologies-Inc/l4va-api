33d485f1a1c86bd9e88b9bd1f05d5bbb21c96664
Anton Kotyk <kotik.anton.md@gmail.com>
2025-06-02 17:36:30 +0300

    Fixed contribution

--- src/modules/blockchain/vault-inserting.service.ts
+++ src/modules/blockchain/vault-inserting.service.ts
@@ -97,9 +97,9 @@ export class VaultInsertingService {
       const txDetail = await this.blockchainScanner.getTransactionDetails(vault.publication_hash);
 
       const { output_amount } = txDetail;
-      this.logger.log(JSON.stringify(output_amount[1].unit));
+      this.logger.log(JSON.stringify(output_amount[output_amount.length -1 ].unit));
 
-      const vaultPolicyPlusName = output_amount[1].unit;
+      const vaultPolicyPlusName = output_amount[output_amount.length - 1].unit;
       const VAULT_POLICY_ID = vaultPolicyPlusName.slice(0,56);
       const VAULT_ID = vaultPolicyPlusName.slice(56,vaultPolicyPlusName.length);
 
