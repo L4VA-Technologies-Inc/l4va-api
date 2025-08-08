eb3a2beedaca0463d510653ef4320f9f9e0e8b08
Artem Trehub <tregubartemofficial@gmail.com>
2025-08-08 16:28:10 +0300

    feat(users): update UsersModule and UsersService to include Vault and Asset repositories, and enhance user profile with total vaults and TVL calculations

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -6,12 +6,14 @@ import { AwsModule } from '../aws_bucket/aws.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
+import { Asset } from '@/database/asset.entity';
 import { FileEntity } from '@/database/file.entity';
 import { LinkEntity } from '@/database/link.entity';
 import { User } from '@/database/user.entity';
+import { Vault } from '@/database/vault.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, FileEntity, LinkEntity]), AwsModule],
+  imports: [TypeOrmModule.forFeature([User, FileEntity, LinkEntity, Vault, Asset]), AwsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
