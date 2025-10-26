a02914f9dceb21add609de28c43e512bffa23bd8
eduardo <eduardgusak552@gmail.com>
2025-10-26 11:40:33 +0200

    UPDATED: token decimal

--- src/modules/vaults/phase-management/contribution/contribution.service.ts
+++ src/modules/vaults/phase-management/contribution/contribution.service.ts
@@ -167,7 +167,7 @@ export class ContributionService {
               description: vault.description,
               ticker: vault.vault_token_ticker,
               logo: vault.ft_token_img?.file_url || '',
-              decimals: vault.ft_token_decimals || 6,
+              decimals: vault.ft_token_decimals || 3,
             });
           } catch (error) {
             this.logger.error('Error updating vault metadata:', error);
