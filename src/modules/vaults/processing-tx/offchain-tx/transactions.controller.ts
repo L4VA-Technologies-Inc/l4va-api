e67a2f8c12e9b93b54c3e4f5d12d001fc986d330
eduardo <eduardgusak552@gmail.com>
2025-11-10 12:34:58 +0200

    FIXED: transactions controler

--- src/modules/vaults/processing-tx/offchain-tx/transactions.controller.ts
+++ src/modules/vaults/processing-tx/offchain-tx/transactions.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Param, Query, Request, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 
-import { AuthGuard } from '../../../auth/auth.guard';
+import { AuthGuard } from "../../../auth/auth.guard";
 import { TransactionsService } from "@/modules/vaults/processing-tx/offchain-tx/transactions.service";
 import { AuthRequest } from "@/modules/auth/dto/auth-user.interface";
 import { GetTransactionsDto } from "@/modules/vaults/processing-tx/offchain-tx/dto/get-transactions.dto";
@@ -11,10 +18,9 @@ import { GetTransactionsDto } from "@/modules/vaults/processing-tx/offchain-tx/d
 @UseGuards(AuthGuard)
 @ApiBearerAuth()
 export class TransactionsController {
-  constructor(private readonly transactionsService: TransactionsService) {
-  }
+  constructor(private readonly transactionsService: TransactionsService) {}
 
-  @Get('')
+  @Get()
   @ApiOperation({ summary: 'Get user transactions' })
   async getByUserId(@Request() req: AuthRequest, @Query() query: GetTransactionsDto) {
     const userId = req.user.sub;
@@ -29,14 +35,16 @@ export class TransactionsController {
   }
 
   @Get('received')
-  @ApiOperation({ summary: 'Get transactions received by the authenticated user' })
+  @ApiOperation({
+    summary: 'Get transactions received by the authenticated user',
+  })
   async getReceivedTransactions() {
     // return this.transactionsService.getTransactionsByReceiver(address);
     return null;
   }
   @Get(':txHash')
   @ApiOperation({ summary: 'Get transaction details by transaction hash' })
-  async getTransaction(@Param('txHash') _txHash: string) {
+  async getTransaction(@Param("txHash") _txHash: string) {
     //return this.transactionsService.getTransaction(txHash);
     return null;
   }
