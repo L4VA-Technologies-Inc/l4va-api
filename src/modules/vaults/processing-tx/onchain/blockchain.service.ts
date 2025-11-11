58587c0c3da9833ac2ab1ab99c6aecd3c7923af3
eduardo <eduardgusak552@gmail.com>
2025-11-11 15:00:58 +0200

    FIXED: reviewers issue

--- src/modules/vaults/processing-tx/onchain/blockchain.service.ts
+++ src/modules/vaults/processing-tx/onchain/blockchain.service.ts
@@ -515,16 +515,10 @@ export class BlockchainService {
 
     while (Date.now() - startTime < maxWaitTime) {
       try {
-        try {
-          const txDetails = await this.blockfrost.txs(txHash);
-          if (txDetails && txDetails.block_height) {
-            await new Promise(resolve => setTimeout(resolve, 80000)); // Wait for 2 blocks before reusing its outputs
-            return true;
-          }
-        } catch (blockfrostError) {
-          if (blockfrostError) {
-            console.log();
-          }
+        const txDetails = await this.blockfrost.txs(txHash);
+        if (txDetails && txDetails.block_height) {
+          await new Promise(resolve => setTimeout(resolve, 80000)); // Wait for 2 blocks before reusing its outputs
+          return true;
         }
 
         // Wait before next check
