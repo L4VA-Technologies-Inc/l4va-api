66b2a9262a828bf240a031da17966f14961f34f3
Artem Trehub <tregubartemofficial@gmail.com>
2025-11-25 11:53:08 +0200

    feat(contribution): add ContributionInput interface to manage contribution details

--- src/modules/vaults/processing-tx/onchain/vault-inserting.service.ts
+++ src/modules/vaults/processing-tx/onchain/vault-inserting.service.ts
@@ -15,10 +15,11 @@ import { SubmitTransactionDto } from './dto/transaction.dto';
 import { BlockchainWebhookDto } from './dto/webhook.dto';
 import { ValidityIntervalException } from './exceptions/validity-interval.exception';
 import { OnchainTransactionStatus } from './types/transaction-status.enum';
-import { Datum, Redeemer } from './types/type';
+import { Redeemer } from './types/type';
 import { getUtxosExtract } from './utils/lib';
 
 import { Vault } from '@/database/vault.entity';
+import { ContributionInput } from '@/modules/distribution/distribution.types';
 import { TransactionStatus } from '@/types/transaction.types';
 
 // Acquire and Contribution
@@ -56,35 +57,6 @@ export interface TransactionBuildResponse {
 
 export interface TransactionSubmitResponse {
   txHash: string;
-}
-
-interface ContributionInput {
-  changeAddress: string;
-  utxos: string[];
-  message: string;
-  mint: object[];
-  scriptInteractions: object[];
-  outputs: (
-    | {
-        address: string;
-        assets?: object[];
-        lovelace?: number; // Required if Contribution in ADA
-        datum?: { type: 'inline'; value: Datum; shape: object };
-      }
-    // Fee output
-    | {
-        address: string;
-        lovelace: number;
-      }
-  )[];
-  requiredSigners: string[];
-  requiredInputs?: string[];
-  referenceInputs: { txHash: string; index: number }[];
-  validityInterval: {
-    start: boolean;
-    end: boolean;
-  };
-  network: string;
 }
 
 @Injectable()
