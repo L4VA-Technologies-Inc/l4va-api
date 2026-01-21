7c3424a3d11d924d35ea0a36c92435474ebc6d37
Artem Trehub <tregubartemofficial@gmail.com>
2026-01-21 16:41:18 +0200

    UPDATED: add Snapshot entity to AssetsModule imports

--- src/modules/vaults/assets/assets.module.ts
+++ src/modules/vaults/assets/assets.module.ts
@@ -6,12 +6,13 @@ import { AssetsService } from './assets.service';
 
 import { Asset } from '@/database/asset.entity';
 import { Claim } from '@/database/claim.entity';
+import { Snapshot } from '@/database/snapshot.entity';
 import { Transaction } from '@/database/transaction.entity';
 import { User } from '@/database/user.entity';
 import { Vault } from '@/database/vault.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Asset, Vault, Transaction, User, Claim])],
+  imports: [TypeOrmModule.forFeature([Asset, Vault, Transaction, User, Claim, Snapshot])],
   controllers: [AssetsController],
   providers: [AssetsService],
   exports: [AssetsService],
