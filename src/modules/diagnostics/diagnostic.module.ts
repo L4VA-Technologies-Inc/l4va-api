15edd67d937d8c9df58cb46e8cfd5024880942ed
Artem Trehub <tregubartemofficial@gmail.com>
2026-02-12 15:35:53 +0200

    Remove DiagnosticController from DiagnosticModule

--- src/modules/diagnostics/diagnostic.module.ts
+++ src/modules/diagnostics/diagnostic.module.ts
@@ -4,8 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DistributionModule } from '../distribution/distribution.module';
 import { BlockchainModule } from '../vaults/processing-tx/onchain/blockchain.module';
 
-import { DiagnosticController } from './diagnostic.controller';
-
 import { Claim } from '@/database/claim.entity';
 import { Transaction } from '@/database/transaction.entity';
 import { Vault } from '@/database/vault.entity';
@@ -18,6 +16,5 @@ import { Vault } from '@/database/vault.entity';
  */
 @Module({
   imports: [DistributionModule, BlockchainModule, TypeOrmModule.forFeature([Vault, Claim, Transaction])],
-  controllers: [DiagnosticController],
 })
 export class DiagnosticModule {}
