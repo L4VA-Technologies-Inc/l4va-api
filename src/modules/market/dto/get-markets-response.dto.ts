d684f5cc616c36c038c87e5b9ffe3ec2e8384291
Misha <gumanist225@gmail.com>
2026-03-10 11:24:06 +0200

    ADDED: fdv per asset

--- src/modules/market/dto/get-markets-response.dto.ts
+++ src/modules/market/dto/get-markets-response.dto.ts
@@ -15,6 +15,7 @@ export interface MarketItem {
   price_change_7d: number;
   price_change_30d: number;
   delta: number | null;
+  fdv_per_asset: number | null;
   created_at: Date;
   updated_at: Date;
   ticker: string | null;
