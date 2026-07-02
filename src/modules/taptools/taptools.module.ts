f941ab818f3eb42002b945c965b81d719052f23f
Artem Trehub <tregbuartemofficial@gmail.com>
2026-07-02 17:44:32 +0300

    ENHANCED: Add Charli3 pricing integration for VLRM token and adjust TVL update intervals

--- src/modules/taptools/taptools.module.ts
+++ src/modules/taptools/taptools.module.ts
@@ -12,6 +12,7 @@ import { Snapshot } from '@/database/snapshot.entity';
 import { User } from '@/database/user.entity';
 import { Vault } from '@/database/vault.entity';
 import { AlertsModule } from '@/modules/alerts/alerts.module';
+import { Charli3PricingModule } from '@/modules/charli3/charli3-pricing.module';
 import { DexHunterPricingModule } from '@/modules/dexhunter/dexhunter-pricing.module';
 import { RedisModule } from '@/modules/redis/redis.module';
 import { TapToolsPricingModule } from '@/modules/taptools/taptools-pricing.module';
@@ -23,6 +24,7 @@ import { WayUpPricingModule } from '@/modules/wayup/wayup-pricing.module';
     TypeOrmModule.forFeature([Vault, User, Asset, Snapshot, Market]),
     AlertsModule,
     AssetsModule,
+    Charli3PricingModule,
     DexHunterPricingModule,
     RedisModule,
     TapToolsPricingModule,
