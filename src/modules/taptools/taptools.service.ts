3f3654710872ee1ae9219bd5af2bb895e62a8aad
Artem Trehub <tregbuartemofficial@gmail.com>
2026-07-21 19:45:05 +0300

    fix: add eslint directive to suppress unused variable warning for ERC721_TRANSFER_TOPIC

--- src/modules/taptools/taptools.service.ts
+++ src/modules/taptools/taptools.service.ts
@@ -69,6 +69,7 @@ const ERC721_INTERFACE_ID = '0x80ac58cd' as const;
 const ERC1155_INTERFACE_ID = '0xd9b67a26' as const;
 
 // Transfer event topic0 values for token enumeration
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ERC721_TRANSFER_TOPIC = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' as const;
 const ERC1155_TRANSFER_SINGLE_TOPIC = '0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62' as const;
 const ERC1155_TRANSFER_BATCH_TOPIC = '0x4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb' as const;
